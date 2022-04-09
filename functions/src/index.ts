549c19a4a2ef95534f663e908a99e874dd7d6164
nguyenvukhang <brew4k@gmail.com>
2022-04-09 09:50:07 +0800

    update lint preferences

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,25 +1,25 @@
-import * as functions from "firebase-functions";
-import {initializeApp} from "firebase-admin/app";
-import {firestore} from "firebase-admin";
+import * as functions from 'firebase-functions'
+import { initializeApp } from 'firebase-admin/app'
+import { firestore } from 'firebase-admin'
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 
-initializeApp();
+initializeApp()
 
 export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+  functions.logger.info('Hello logs!', { structuredData: true })
+  response.send('Hello from Firebase!')
+})
 
 export const addMessage = functions.https.onRequest(async (req, res) => {
   // Grab the text parameter.
-  const original = req.query.text;
+  const original = req.query.text
   // Push the new message into Firestore using the Firebase Admin SDK.
   const writeResult = await firestore()
-      .collection("cloud-functions")
-      .add({original: original});
+      .collection('cloud-functions')
+      .add({ original: original })
   // Send back a message that we've successfully written the message
-  res.json({result: `Message with ID: ${writeResult.id} added.`});
-});
+  res.json({ result: `Message with ID: ${writeResult.id} added.` })
+})
 
