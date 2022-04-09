fd84815dc14ce5faebcc401150ec04eef6696130
nguyenvukhang <brew4k@gmail.com>
2022-04-09 16:25:57 +0800

    more prep

--- functions/src/handleMatch/index.ts
+++ functions/src/handleMatch/index.ts
@@ -2,7 +2,18 @@ import { https } from 'firebase-functions'
 import { firestore } from 'firebase-admin'
 import { IncomingHandleMatchRequest, ServerMatchRecord } from '@/types/index'
 
-const saveMatchRecordAsync = async (data: ServerMatchRecord) => {
+const saveMatchRecordAsync = async (data: IncomingHandleMatchRequest) => {
+  // get timestamp from server
+  const timestamp: Date = firestore.Timestamp.now().toDate()
+  const serverMatchRecord: ServerMatchRecord = { ...data, timestamp }
+
+  const writeResult = await firestore()
+    .collection('match-records')
+    .add(serverMatchRecord)
+  return writeResult
+}
+
+const updateKnockoutTable = async (data: IncomingHandleMatchRequest) => {
   const writeResult = await firestore().collection('match-records').add(data)
   return writeResult
 }
@@ -21,14 +32,7 @@ export const handleMatch = https.onRequest(async (req, res) => {
     facilitatorEmail: 'hongsheng@gmail.com',
   }
 
-  // get timestamp from server
-  const timestamp: Date = firestore.Timestamp.now().toDate()
-
-  const serverMatchRecord: ServerMatchRecord = {...request, timestamp}
-
-  const [writeResult] = await Promise.all([
-    saveMatchRecordAsync(serverMatchRecord),
-  ])
+  const [writeResult] = await Promise.all([saveMatchRecordAsync(request)])
 
   // Send back a message that we've successfully written the match
   res.json({ result: `Match with ID: ${writeResult.id} written.` })
