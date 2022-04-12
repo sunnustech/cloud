176690a0b68a8c07b4306da70b83991874e3b9c7
nguyenvukhang <brew4k@gmail.com>
2022-04-12 23:35:09 +0800

    boutta drop a whole bunch of users

--- functions/src/tests/src/createUsers.ts
+++ functions/src/tests/src/createUsers.ts
@@ -3,6 +3,8 @@ import { timestamp } from './timestamp'
 import { cloud } from './firebase'
 import { sanitizePhoneNumber } from './utils'
 import { RequestUser } from '../../types/users'
+import { parse } from 'csv-parse/sync'
+import fs from 'fs'
 
 const createUser = (user: RequestUser): RequestUser => {
   return {
@@ -17,11 +19,22 @@ const csv: RequestUser[] = [
   { email: '2@gmail.com', phoneNumber: '+65 91212368', teamName: 'auto_magic' },
 ]
 
+/* read from the csv file */
+const fileData = fs.readFileSync('src/csv/users.csv')
+const data: string[][] = parse(fileData, {
+  delimiter: ',',
+  trim: true,
+  columns: true,
+})
+
+console.log('data:', data)
+
 const sanitizedUserList: RequestUser[] = csv.map((user) => createUser(user))
 
 const fn = 'createUsers'
 timestamp(fn)
-axios.post(cloud(fn), { userList: sanitizedUserList }).then((res) => {
-  const data = res.data
-  console.log(data)
-})
+
+// axios.post(cloud(fn), { userList: sanitizedUserList }).then((res) => {
+//   const data = res.data
+//   console.log(data)
+// })
