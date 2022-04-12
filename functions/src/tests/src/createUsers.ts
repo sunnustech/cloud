72aabd45ae629c15728189947d4601e1b96f8ca9
nguyenvukhang <brew4k@gmail.com>
2022-04-12 23:37:50 +0800

    createUsers now imports from a csv

--- functions/src/tests/src/createUsers.ts
+++ functions/src/tests/src/createUsers.ts
@@ -14,27 +14,20 @@ const createUser = (user: RequestUser): RequestUser => {
   }
 }
 
-const csv: RequestUser[] = [
-  { email: '1@gmail.com', phoneNumber: '9832 6742', teamName: 'auto_matic' },
-  { email: '2@gmail.com', phoneNumber: '+65 91212368', teamName: 'auto_magic' },
-]
-
 /* read from the csv file */
 const fileData = fs.readFileSync('src/csv/users.csv')
-const data: string[][] = parse(fileData, {
+const csv: RequestUser[] = parse(fileData, {
   delimiter: ',',
   trim: true,
   columns: true,
 })
-
-console.log('data:', data)
 
 const sanitizedUserList: RequestUser[] = csv.map((user) => createUser(user))
 
 const fn = 'createUsers'
 timestamp(fn)
 
-// axios.post(cloud(fn), { userList: sanitizedUserList }).then((res) => {
-//   const data = res.data
-//   console.log(data)
-// })
+axios.post(cloud(fn), { userList: sanitizedUserList }).then((res) => {
+  const data = res.data
+  console.log(data)
+})
