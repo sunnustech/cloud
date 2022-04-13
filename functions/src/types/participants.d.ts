d30b8e4a7b7c755b18aedec6d7a2efd1eb41b1e3
nguyenvukhang <brew4k@gmail.com>
2022-04-13 09:20:00 +0800

    removed participants.d.ts

--- functions/src/types/participants.d.ts
+++
@@ -1,7 +0,0 @@
-import { Team } from './sunnus-firestore'
-
-/*
- * To be Firestore-friendly, the final form has to be an object,
- * and first-level values cannot be arrays
- */
-type TeamsDatabase = Record<string, Team>
