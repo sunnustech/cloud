dfab7a6b14403a1a3e5f7867d5a8c72b0d5d61cf
nguyenvukhang <brew4k@gmail.com>
2022-04-13 12:22:37 +0800

    split production and development functions

---
+++ functions/src/development.ts
@@ -0,0 +1,40 @@
+import { firestore } from 'firebase-admin'
+import { https } from 'firebase-functions'
+
+export const addMessage = https.onRequest(async (req, res) => {
+  // Grab the text parameter.
+  const original = req.query.text
+  // Push the new message into Firestore using the Firebase Admin SDK.
+  const writeResult = await firestore()
+    .collection('cloud-functions')
+    .add({ original: original })
+  // Send back a message that we've successfully written the message
+  res.json({ result: `Message with ID: ${writeResult.id} added.` })
+})
+
+export const helloWorld = https.onRequest(async (req, res) => {
+  console.log('hello, server!')
+  res.json({
+    message: 'hello, requester!',
+    serverReceived: req.body,
+  })
+})
+
+/* sunnus functions */
+import { handleMatch } from './roundRobin'
+import { writeSchema } from './data/writeSchema'
+import { createTeams } from './teams/createTeams'
+import { createUsers } from './users/createUsers'
+import { assignUsers } from './users/assignUsers'
+import { deleteAllUsers } from './users/deleteAllUsers'
+import { deleteDocs } from './utils'
+
+export {
+  deleteDocs,
+  createTeams,
+  handleMatch,
+  writeSchema,
+  deleteAllUsers,
+  createUsers,
+  assignUsers,
+}
---
+++ functions/src/production.ts
@@ -0,0 +1,22 @@
+import { logger, https } from 'firebase-functions'
+import { AuthData } from 'firebase-functions/lib/common/providers/https'
+
+export const authTest = https.onCall((data, context) => {
+  logger.info('authentication testing 3')
+
+  if (!context.auth) {
+    throw new https.HttpsError('unauthenticated', 'Please log in first')
+  }
+
+  /* use this section to check for user type (participant/facil/admin)
+   * use email as identifier and lookup in database for roles
+   */
+  const auth: AuthData | undefined = context.auth
+  const email = auth.token.email
+
+  return {
+    email,
+    originalData: data,
+    hello: 'world',
+  }
+})
