b1587d5c1082e20712c268300894bfc42eb59468
nguyenvukhang <brew4k@gmail.com>
2022-04-14 16:43:39 +0800

    moved schedule maker out

--- functions/src/schedule/createSchedule.ts
+++ functions/src/schedule/createSchedule.ts
@@ -1,19 +1,11 @@
 import { https } from 'firebase-functions'
-import { Sport } from '../types/schedule'
+import { Event, RoundRobinConfig, ScheduleConfig } from '../types/schedule'
 import { hasMissingKeys } from '../utils'
 import { createSchedule as keyCheck } from '../utils/keyChecks'
+import { makeSchedule } from './makeSchedule'
 // import { firestore } from 'firebase-admin'
 // import { getAuth, UserRecord } from 'firebase-admin/auth'
 // import { WriteResult } from '@google-cloud/firestore'
-
-const sports: Sport[] = [
-  'touchRugby',
-  'dodgeball',
-  'frisbee',
-  'tchoukball',
-  'volleyball',
-  'captainsBall',
-]
 
 export const createSchedule = https.onRequest(async (req, res) => {
   const [err, status] = hasMissingKeys(keyCheck, req)
@@ -21,8 +13,12 @@ export const createSchedule = https.onRequest(async (req, res) => {
     res.json({ message: err })
     return
   }
+  const sc: ScheduleConfig = req.body.scheduleConfig
+  const rr: RoundRobinConfig = req.body.roundRobinConfig
 
-  console.log(sports)
+  const schedule: Event[] = makeSchedule(sc, rr)
+
+  console.log(schedule)
 
   /* send back the statuses */
   res.json({
---
+++ functions/src/schedule/makeSchedule.ts
@@ -0,0 +1,111 @@
+import {
+  RoundRobinConfig,
+  ScheduleConfig,
+  Sport,
+  Event,
+} from '../types/schedule'
+
+const sports: Sport[] = [
+  'touchRugby',
+  'dodgeball',
+  'frisbee',
+  'tchoukball',
+  'volleyball',
+  'captainsBall',
+]
+
+function dateify(HHMM: string): Date {
+  const [h, m] = HHMM.split(':').map((e) => parseInt(e))
+  return new Date(0, 0, 0, h, m)
+}
+
+function inBetween(c: Date, a: Date, b: Date): boolean {
+  const [C, A, B] = [c, a, b].map((e: Date) => e.getTime())
+  return A <= C && C < B
+}
+
+function letter(n: number): string {
+  if (n < 0 || n > 25) {
+    return '_'
+  }
+  return (n + 10).toString(36).toUpperCase()
+}
+
+function time(t: Date): string {
+  return t.toLocaleTimeString('en-sg', {
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+  })
+}
+
+function startEndInit(first: Date, matchLength: number) {
+  const s = new Date(first)
+  const e = new Date(first)
+  e.setMinutes(s.getMinutes() + matchLength)
+  return [s, e]
+}
+
+function incrementTime(s: Date, e: Date, interval: number): void {
+  s.setMinutes(s.getMinutes() + interval)
+  e.setMinutes(e.getMinutes() + interval)
+}
+
+export const makeSchedule = (
+  scheduleConfig: ScheduleConfig,
+  rr: RoundRobinConfig
+): Event[] => {
+  const schedule: Event[] = []
+
+  sports.forEach((sport) => {
+    const config = scheduleConfig[sport]
+    const matches: number[][] = rr[config.density]
+    const ls = dateify(config.lunchStart)
+    const le = dateify(config.lunchEnd)
+    const courts = config.courts
+    courts.forEach((court, groupIndex) => {
+      // first pair of start and end
+      const first = dateify(config.startTime)
+      const [s, e] = startEndInit(first, config.matchLength)
+
+      matches.forEach((match) => {
+        // timeskip through lunch break
+        const skipToAfterLunch = () => {
+          const duringLunch = (t: Date) => inBetween(t, ls, le)
+          if (duringLunch(s) || duringLunch(e)) {
+            s.setTime(new Date(le).getTime())
+            e.setTime(new Date(le).getTime())
+            e.setMinutes(le.getMinutes() + config.matchLength)
+          }
+        }
+        const inc = () => incrementTime(s, e, config.matchInterval)
+        const add = (groupIndex: number) =>
+          schedule.push({
+            start: time(s),
+            end: time(e),
+            venue: config.venue,
+            round: 'round_robin',
+            court,
+            sport,
+            A: `${letter(groupIndex)}${match[0]}`,
+            B: `${letter(groupIndex)}${match[1]}`,
+            winner: 'U',
+          })
+        if (config.alternating) {
+          skipToAfterLunch()
+          add(2 * groupIndex)
+          inc()
+          skipToAfterLunch()
+          add(2 * groupIndex + 1)
+          inc()
+        } else {
+          skipToAfterLunch()
+          add(groupIndex)
+          inc()
+        }
+      })
+    })
+  })
+
+  return schedule
+}
