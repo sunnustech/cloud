d5532acdb524ed3b3595dfc67b666f48224bd149
nguyenvukhang <brew4k@gmail.com>
2022-04-27 15:56:19 +0800

    remove dup

--- functions/src/teams/createTeams.ts
+++ functions/src/teams/createTeams.ts
@@ -1,39 +1,10 @@
 import { https } from 'firebase-functions'
 import { InitializeTeam } from '../types/sunnus-init'
-import { Team } from '../types/sunnus-firestore'
 import { firestore } from 'firebase-admin'
 import { WriteResult } from '@google-cloud/firestore'
 import { hasMissingKeys } from '../utils'
 import { createTeams as keyCheck } from '../utils/keyChecks'
-
-/**
- * creates a firestore-ready team from request data
- * @param {InitializeTeam} props: the request data
- * @return {Team} a firestore-ready team object
- */
-function makeTeam(props: InitializeTeam): Team {
-  return {
-    teamName: props.teamName,
-    registeredEvents: props.registeredEvents,
-    direction: props.direction,
-    SOAR: {
-      direction: props.direction,
-      allEvents: [],
-      points: 0,
-      startTime: 0,
-      started: false,
-      stopTime: 0,
-      stopped: false,
-      timerRunning: false,
-    },
-    SOARPausedAt: 0,
-    SOARStart: 0,
-    SOARStationsCompleted: [],
-    SOARStationsRemaining: [],
-    SOARTimerEvents: [0],
-    members: [],
-  }
-}
+import { makeTeam } from '../utils/team'
 
 export const createTeams = https.onRequest(async (req, res) => {
   const [err, status] = hasMissingKeys(keyCheck, req)
--- functions/src/utils/team.ts
+++ functions/src/utils/team.ts
@@ -1,3 +1,4 @@
+import { InitializeTeam } from '../types/sunnus-init'
 import { Team } from '../types/sunnus-firestore'
 
 /**
@@ -13,3 +14,33 @@ export function makeTeams(arr: Array<Team>): Record<string, Team> {
   })
   return obj
 }
+
+/**
+ * creates a firestore-ready team from request data
+ * @param {InitializeTeam} props: the request data
+ * @return {Team} a firestore-ready team object
+ */
+export function makeTeam(props: InitializeTeam): Team {
+  return {
+    teamName: props.teamName,
+    registeredEvents: props.registeredEvents,
+    direction: props.direction,
+    SOAR: {
+      direction: props.direction,
+      allEvents: [],
+      points: 0,
+      startTime: 0,
+      started: false,
+      stopTime: 0,
+      stopped: false,
+      timerRunning: false,
+    },
+    SOARPausedAt: 0,
+    SOARStart: 0,
+    SOARStationsCompleted: [],
+    SOARStationsRemaining: [],
+    SOARTimerEvents: [0],
+    members: [],
+  }
+}
+
