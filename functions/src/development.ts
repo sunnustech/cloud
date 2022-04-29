50f34b932244adfef9977a2e73d102153e40cdaa
nguyenvukhang <brew4k@gmail.com>
2022-04-29 23:16:21 +0800

    remove unused example

--- functions/src/development.ts
+++ functions/src/development.ts
@@ -1,16 +1,4 @@
-import { firestore } from 'firebase-admin'
 import { https } from 'firebase-functions'
-
-export const addMessage = https.onRequest(async (req, res) => {
-  // Grab the text parameter.
-  const original = req.query.text
-  // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await firestore()
-    .collection('cloud-functions')
-    .add({ original: original })
-  // Send back a message that we've successfully written the message
-  res.json({ result: `Message with ID: ${writeResult.id} added.` })
-})
 
 export const helloWorld = https.onRequest(async (req, res) => {
   console.debug('hello, server!')
