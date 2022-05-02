1111d29f5c850045d5b34cff225350e52613bc8b
nguyenvukhang <brew4k@gmail.com>
2022-05-02 23:01:17 +0800

    reject incorrect team transfers

--- functions/src/users/trigger.ts
+++ functions/src/users/trigger.ts
@@ -22,19 +22,29 @@ export const autoLinkNewUser = fs
 
 export const autoLinkChangedUser = fs
   .document('users/{uid}')
-  .onUpdate((change, context) => {
-    const data = change.after.data()
-    if (!data) {
-      return null
-    }
-    const teamName = data.teamName
-    if (!teamName) {
-      return null
-    }
+  .onUpdate(async (change, context) => {
     const teamsCollection = firestore().collection('teams')
-    const teamDoc = teamsCollection.doc(teamName)
-    teamDoc.update({
-      members: firestore.FieldValue.arrayUnion(context.params.uid),
-    })
+    // add to new team
+    const after = change.after.data()?.teamName
+    if (after) {
+      const afterTeam = teamsCollection.doc(after)
+      // check if after team exists
+      const doc = await afterTeam.get()
+      if (!doc.exists) {
+        firestore().collection('users').doc(context.params.uid).update(change.before.data())
+        return null
+      }
+      afterTeam.update({
+        members: firestore.FieldValue.arrayUnion(context.params.uid),
+      })
+    }
+    // remove from old team, but only if new team exists
+    const before = change.before.data()?.teamName
+    if (before) {
+      const beforeTeam = teamsCollection.doc(before)
+      beforeTeam.update({
+        members: firestore.FieldValue.arrayRemove(context.params.uid),
+      })
+    }
     return null
   })
